a261039a00aeb6f3e3a492d110a7cdd69fa7dbe4
이은열 <dmsduf102356@gmail.com>
2022-03-24 14:58:52 +0900

    [#4] typeorm 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -15,6 +16,25 @@ import { ConfigModule } from '@nestjs/config';
           ? '.env.test'
           : null,
       isGlobal: true,
+    }),
+    //DB 연결을 위한 세팅
+    TypeOrmModule.forRootAsync({
+      useFactory: () => {
+        return {
+          type: 'mysql',
+          host: process.env.DB_HOST,
+          port: Number(process.env.DB_PORT),
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_NAME,
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: false,
+          logging: process.env.DB_LOGGING === 'true',
+          autoLoadEntities: true,
+          bigNumberStrings: false,
+          timezone: '+09:00',
+        };
+      },
     }),
   ],
   controllers: [AppController],
